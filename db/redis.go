d5ff7223345eefdc980f0983424dd81b5b00404e
Priydarshi Singh <dryairship@gmail.com>
2020-05-23 03:17:22 +0530

    Use custom struct as Redis value

--- db/redis.go
+++ db/redis.go
@@ -1,20 +1,38 @@
 package db
 
 import (
+	"encoding/json"
 	"log"
+
+	"github.com/dryairship/messenger-quiz-bot/models"
 )
 
-func SetUserData(user, data string) {
-	err := redisClient.Set(user, data, 0).Err()
+func SetRedisUserData(user string, data *models.RedisUserData) {
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		log.Println("[ERROR] Can't JSON marshal Redis user data. Error: ", err)
+		return
+	}
+
+	err = redisClient.Set(user, jsonData, 0).Err()
 	if err != nil {
 		log.Println("[ERROR] Redis can't set user data. Error: ", err)
 	}
 }
 
-func GetUserData(user string) string {
-	data, err := redisClient.Get(user).Result()
+func GetRedisUserData(user string) (*models.RedisUserData, error) {
+	jsonData, err := redisClient.Get(user).Result()
 	if err != nil {
 		log.Println("[ERROR] Redis can't get user data. User: ", user, ", Error: ", err)
+		return nil, err
 	}
-	return data
+
+	var data models.RedisUserData
+	err = json.Unmarshal([]byte(jsonData), &data)
+	if err != nil {
+		log.Println("[ERROR] Cannot unmarshal Redis bytes to Redis user data. User: ", user, ", Error: ", err)
+		return nil, err
+	}
+
+	return &data, nil
 }
--- models/user.go
+++ models/user.go
@@ -1,5 +1,20 @@
 package models
 
+type UserState int
+
+const (
+	USER_STATE_ACTIVE_QUESTION UserState = iota
+	USER_STATE_WANT_QUESTION
+	USER_STATE_IDLE
+)
+
 type User struct {
 	Id string `json:"id"`
+}
+
+type RedisUserData struct {
+	State             UserState
+	CorrectAnswerText string
+	CorrectAnswerChar byte
+	MaxAcceptableChar byte
 }
