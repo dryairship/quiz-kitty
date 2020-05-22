f0edcf3c1a6755cdb3daa6cf1e4f167daf243d1c
Priydarshi Singh <dryairship@gmail.com>
2020-05-22 08:00:19 +0530

    Add config package

---
+++ config/config.go
@@ -0,0 +1,25 @@
+package config
+
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+var PORT string
+var CORRECT_TOKEN string
+
+func init() {
+	viper.SetConfigName("quiz-bot-config")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Println("[WARNING] Unable to locate configuration file")
+	}
+
+	viper.SetEnvPrefix("quizbot")
+	viper.AutomaticEnv()
+
+	PORT = viper.GetString("port")
+	CORRECT_TOKEN = viper.GetString("correct_token")
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/dryairship/messenger-quiz-bot
 
 go 1.14
+
+require (
+	github.com/spf13/viper v1.7.0
+)
